b575f3112796b2bf5e4dfb3a3e63f4f3b135f865
Bruno_Barros <barrosbrunofelipe@gmail.com>
2022-08-15 20:40:02 -0300

    refactor: ajuste palavra título tela sign in

--- src/Components/Banner/index.jsx
+++ src/Components/Banner/index.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
     return (
         <Container>
             <h1>SIGHA</h1>
-            <h2>salve, gerencie e fique por dentro das suas horas acadêmias</h2>
+            <h2>salve, gerencie e fique por dentro das suas horas acadêmicas</h2>
         </Container>
     );
 }
